1349ff0bd2a3a6b160778e31200a51ff225f2545
Tobias <Tobias@LAPTOP-A69FKECL>
2016-09-28 15:04:17 -0400

    AccountTest class created. not yet functional

--- src/ps3/Account.java
+++ src/ps3/Account.java
@@ -1,5 +1,61 @@
 package ps3;
+import java.util.Date;
 
 public class Account {
+	private int id = 0;
+	private double balance = 0;
+	private double annualInterestRate = 0;
+	private Date dateCreated;
+	
+	Account(){
+	}
+	
+	Account(int newID, double newBal, double newRate){
+		this.id = newID;
+		this.balance = newBal;
+		this.annualInterestRate = newRate;
+		this.dateCreated = new Date();
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+
+	public void setAnnualInterestRate(double annualInterestRate) {
+		this.annualInterestRate = annualInterestRate;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+	
+	public double getMonthlyInterestRate(){
+		return annualInterestRate / 12;
+	}
+	
+	public double withdraw(double withdrawal){
+		System.out.printf("You withdrew $.%2f", withdrawal);
+		return balance -= withdrawal;
+	}
+	
+	public double deposit(double depositAmt){
+		System.out.printf("You deposited $.%2f", depositAmt);
+		return balance += depositAmt;
+	}
 }
---
+++ src/ps3/AccountTest.java
@@ -0,0 +1,19 @@
+package ps3;
+
+/*
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+*/
+
+public class AccountTest {
+	Account acc1 = new Account(1122, 20000, 4.5);
+	acc1.withdraw(2500);
+	acc1.deposit(3000);
+	
+	
+	//set?
+}
